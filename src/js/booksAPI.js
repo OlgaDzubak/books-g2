959ebceb012ae8a364939a0f48404ceb5c00a432
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-12 18:34:39 +0300

    Update booksAPI.js

--- src/js/booksAPI.js
+++ src/js/booksAPI.js
@@ -4,14 +4,10 @@ export class booksAPI {
 
     // приватні властивості для запиту 
     #BASE_URL = 'https://books-backend.p.goit.global/books/';
-   // #category = '';
 
     //методи классу
     getBookById(book_Id) {console.log(`${this.#BASE_URL}${book_Id}`); return axios.get(`${this.#BASE_URL}${book_Id}`);}
     getTopBooks() {return axios.get(`${this.#BASE_URL}top-books`);}
     getCategoryList() {return axios.get(`${this.#BASE_URL}categoty-list`);}
     getBooksByCategory(category) {return axios.get(`${this.#BASE_URL}categoty`, {params:{category: category}});};
- 
-    // сеттери
-    set category(newCategory) {this.#category = newCategory;};
 }

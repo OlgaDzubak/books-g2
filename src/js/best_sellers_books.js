352977e0111834c3454ac7f1ea964681fe074546
Dima_ <chernyaev603@gmail.com>
2023-05-12 13:00:43 +0300

    Add description
    Description function

--- src/js/best_sellers_books.js
+++ src/js/best_sellers_books.js
@@ -4,24 +4,106 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 //import ...
 
-
+// Змінна що зберігає дів куди добавляти розмітку
 const list = document.querySelector('.best-books')
 list.addEventListener('click', loadMore)
 
 
-const pageWidth = document.documentElement.scrollWidth;
-console.log(pageWidth);
-
+// Функція для запиту за бест бук
 async function fetchBooks () {
     return await axios.get('https://books-backend.p.goit.global/books/top-books');
 }
 
+// Функція для запиту за категорією
 async function fetchCategoryBooks (param) {
     return await axios.get(`https://books-backend.p.goit.global/books/category?category=${param}`);
 }
 
+// Функція що скорочує title i author на книгаг до 17 символів та додає три крапки в кінці
+function shortTitle(string) {
+    if(string.length > 17) {
+        return string.slice(0, 17) + '...';
+    }
+    
+    return string
+}
 
+// Функція для розмітки бест бук
+function createMarcup(arr, querty) {
 
+    const markup = arr.map(({list_name, books}) => {
+        const titleBook = `<p class="theme-book">${list_name}</p>`;
+
+        if (books.length) {
+            let book = books.splice(0, querty).map(({_id, book_image, title, author}) => 
+            `<li class="item-book" data-id="${_id}">
+            <img src="${book_image}" alt="${title}" class="img-book">
+            <p class="title-book">${shortTitle(title)}</p>
+            <p class="author">${shortTitle(author)}</p>
+            </li>`).join('');
+
+            return `${titleBook}<ul class="list-books">${book}</ul>
+            <button type="button" class="button-more js-btn-more" data-category="${list_name}">See more</button>`
+        } else {
+            return `${titleBook}<div class="off-books"><p>Sorry, there are no books in this category, please choose another category</p></div>`
+        }}).join('');
+
+        return `<h2 class="title-theme-book">Best Sellers Books</h2>${markup}`;
+}
+
+// Початкова функція яка робить запит та промальовує бест бук
+async function createBestBook() {
+    const pageWidth = document.documentElement.scrollWidth;
+
+    try {
+        const { data } = await fetchBooks();
+        console.log(data);
+
+        if (data.length) {
+            if (pageWidth >= 1440) {
+                list.innerHTML = createMarcup(data, 5);
+                return
+            }
+        
+            if (pageWidth < 1440 && pageWidth >= 768) {
+                list.innerHTML = createMarcup(data, 3);
+                return
+            }
+        
+            if (pageWidth < 768) {
+                list.innerHTML = createMarcup(data, 1);
+                return
+            }
+        } else {
+            Notify.failure("Sorry, there was a server error, please reload the page")
+            return
+        }
+    } catch (error) {
+        console.error(error);
+        Notify.failure('Sorry, there was a server error, please reload the page');
+    }   
+}
+// Виклик даної функції для промальовки всього при завантажені сторінки
+createBestBook()
+
+// Функція для розмітки книг за категорією
+function createMarcupCategoryBook(arr) {
+if(arr.length){
+        const markup = arr.map(({_id, book_image, author, title}) => 
+        `<li class="item-book" data-id="${_id}">
+        <img src="${book_image}" alt="${title}" class="img-book">
+        <p class="title-book">${shortTitle(title)}</p>
+        <p class="author">${shortTitle(author)}</p>
+        </li>`
+        ).join('')
+
+        return `<h2 class="title-theme-book">${arr[0].list_name}</h2><ul class="list-books category">${markup}</ul>`
+    } else {
+        return `<div class="off-books"><p>Sorry, there are no books in this category, please choose another category</p></div>`
+    }
+}
+
+// Функція яка считує датасет кнопки провіряє його та дає параметер запиту за категорією
 function categoryParam(key) {
     let param = null;
     switch (key) {
@@ -53,7 +135,7 @@ function categoryParam(key) {
             param = 'Paperback%20Nonfiction';
             break;
         case 'Childrens Middle Grade Hardcover':
-            param = 'Childrens%20Middle%20Grade%20Hardcove';
+            param = 'Childrens%20Middle%20Grade%20Hardcover';
             break;
         case 'Young Adult Hardcover':
             param = 'Young%20Adult%20Hardcover';
@@ -87,90 +169,23 @@ function categoryParam(key) {
     return param
 }
 
-
-function shortTitle(string) {
-    if(string.length > 17) {
-        return string.slice(0, 17) + '...';
-    }
-    
-    return string
-}
-
-function createMarcup(arr, querty) {
-
-    const markup = arr.map(({list_name, books}) => {
-        const titleBook = `<p class="theme-book">${list_name}</p>`;
-        let book = books.splice(0, querty).map(({_id, book_image, title, author}) => 
-        `<li class="item-book" data-id="${_id}">
-        <img src="${book_image}" alt="${title}" class="img-book">
-        <p class="title-book">${shortTitle(title)}</p>
-        <p class="author">${shortTitle(author)}</p>
-        </li>`).join('');;
-
-        return `${titleBook}<ul class="list-books">${book}</ul>
-        <button type="button" class="button-more js-btn-more" data-category="${list_name}">See more</button>`}).join('');
-
-        return `<h2 class="title-theme-book">Best Sellers Books</h2>${markup}`;
-}
-
-
-async function createBestBook() {
-    try {
-        const { data } = await fetchBooks();
-        console.log(data);
-    
-        if (!data.length) {
-            Notify.failure("Sorry, we don't have any books.")
-            return
-        } else {
-            if (pageWidth >= 1440) {
-                list.innerHTML = createMarcup(data, 5);
-                return
-            }
-        
-            if (pageWidth < 1440 && pageWidth >= 768) {
-                list.innerHTML = createMarcup(data, 3);
-                return
-            }
-        
-            if (pageWidth < 768) {
-                list.innerHTML = createMarcup(data, 1);
-                return
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        Notify.failure('Пробачте, виникла помилка на сервері');
-    }   
-}
-
-createBestBook()
-
-
-function createMarcupCategoryBook(arr) {
-
-        const markup = arr.map(({_id, book_image, author, title}) => 
-        `<li class="item-book" data-id="${_id}">
-        <img src="${book_image}" alt="${title}" class="img-book">
-        <p class="title-book">${shortTitle(title)}</p>
-        <p class="author">${shortTitle(author)}</p>
-        </li>`
-        ).join('')
-
-        return `<h2 class="title-theme-book">${arr[0].list_name}</h2><ul class="list-books">${markup}</ul>`
-}
-
-
-
+// Функція для здійснення запиту за категорією при нажатті на кнопку load more яка діє за зчиткою датасету з кнопки 
 async function loadMore(event) {
     event.preventDefault();
 
     const { target } = event;
-
-    if(target.classList.contains('js-btn-more')) {
-        const category = target.dataset.category.toString();
-        const param = categoryParam(category);
-        const { data } = await fetchCategoryBooks(param);
-        list.innerHTML = createMarcupCategoryBook(data);
+    try {
+        if(!target.classList.contains('js-btn-more')) {
+            return
+        } else {
+            const category = target.dataset.category.toString();
+            const param = categoryParam(category);
+            const { data } = await fetchCategoryBooks(param);
+            list.innerHTML = createMarcupCategoryBook(data);
+        }
+    } catch (error) {
+        console.error(error);
+        Notify.failure('Sorry, there was a server error, please reload the page');
     }
+
 }

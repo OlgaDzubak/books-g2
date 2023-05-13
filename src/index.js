d4c66055af2ea1531aa99eb4665ec82b6deb1a8c
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-14 01:12:09 +0300

    Update support_ukraine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 
 import './js/list_of_categories'
 import './js/support_ukraine'
+
 import './js/best_sellers_books'
 import './js/category_books'
 import './js/autorization_modal_window'
 import './js/book_modal_window'
 import './js/mobile_menu'
+
 const homeLinkEl = document.querySelector('.js-nav-homelink');
 const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
 homeLinkEl.classList.toggle('selected');
--- src/js/support_ukraine.js
+++ src/js/support_ukraine.js
@@ -1,43 +1,53 @@
-import { supportItems } from './support_ukraie_items';
+// // import { supportItems } from './support_ukraie_items';
 
-const galleryItemsContainer = document.querySelector('.swiper-wrapper');
-const fundsMarkup = createFundsMarkup(supportItems);
+//  //const galleryItemsContainer = document.querySelector('.swiper-wrapper');
 
-galleryItemsContainer.insertAdjacentHTML('beforeend', createFundsMarkup);
+// // galleryItemsContainer.insertAdjacentHTML('beforeend', createFundsMarkup(supportItems));
 
-function numberFounds(arr) {
-  for (let i = 0; i < arr.length; i += 1) {
-    console.log(arr[i]);
-  }
-}
-numberFounds(supportItems);
+// // // function numberFounds(arr) {
+// // //   for (let i = 0; i < arr.length; i += 1) {
+// // //     console.log(arr[i]);
+// // //   }
+// // // }
+// // // numberFounds(supportItems);
 
-function createFundsMarkup(supportItems) {
-  console.log(supportItems.children);
+// // function createFundsMarkup(supportItems) {
 
-  return supportItems.map(({ title, url, img }) => {
-      return `
-            <li class="support_gallery_item swiper-slide">
-              <a class="support_gallery_link link" href="${url}" title="${title}">
-                <svg class="support_gallery_link_svg" height="20">
-                <use href="${img}"></use>
-                </svg>
-              </a>
-            </li>
-            `
-            ;})
-            .join('');
-}
+// //   return supportItems.map((item) => {
+// //       console.log(item.title);
+// //       return `
+// //             <li class="support_gallery_item swiper-slide">
 
-console.log(supportItems);
+// //               <a class="support_gallery_link link" href=${item.url}  target="_blank">
+// //                 <img class="support_gallery_link_svg" src=${item.img} alt=" " loading="lazy"></img>
+// //               </a>
 
-const swiper = new Swiper('.mySwiper', {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  direction: 'vertical',
-  loop: true,
+// //             </li>
+// //             `
+// //             ;})
+// //             .join('');
+// // }
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-  },
-});
+// const swiper = document.querySelector('.swiper');
+// const btn = document.querySelector('.slider_btn');
+// btn.addEventListener('click', Func);
+
+
+// function Func() {
+//   const { height: cardHeight } = swiper.firstElementChild.getBoundingClientRect();
+//   window.scrollBy({top: cardHeight * 2, behavior: "smooth",});
+// }
+
+
+
+
+// // const swiper = new Swiper('.mySwiper', {
+// //   slidesPerView: 4,
+// //   spaceBetween: 20,
+// //   direction: 'vertical',
+// //   loop: true,
+
+// //   navigation: {
+// //     nextEl: '.swiper-button-next',
+// //   },
+// // });

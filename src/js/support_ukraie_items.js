1f7c5a82795eb2ccbb44329a2a3a3289925d7792
Oksana Komarnytska <okazimiriv@gmail.com>
2023-05-16 16:09:28 +0200

    підправида js

--- src/js/support_ukraie_items.js
+++ src/js/support_ukraie_items.js
@@ -10,56 +10,56 @@ import worldVision from '/src/images/png/world_vision_icon.png';
 
 export const supportItems = [
   {
-    id: '01',
+    id: 1,
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: saveTheChildren,
   },
   {
-    id: '02',
+    id: 2,
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
     img: projectHope,
   },
   {
-    id: '03',
+    id: 3,
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
     img: internationalMedicalCorps,
   },
   {
-    id: '04',
+    id: 4,
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
     img: razom,
   },
   {
-    id: '05',
+    id: 5,
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
     img: actionAgainstHunger,
   },
   {
-    id: '06',
+    id: 6,
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
     img: serhiyPrytulaFoundation,
   },
   {
-    id: '07',
+    id: 7,
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
     img: united24,
   },
 
   {
-    id: '08',
+    id: 8,
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
     img: medicinsSansFrontieres,
   },
   {
-    id: '09',
+    id: 9,
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
     img: worldVision,

da3b2944ae3a9e1094f2bd18ac65cd86dec0c153
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-16 14:32:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './js/support_ukraine';
 import './js/best_sellers_books';
 import './js/category_books';
 import './js/autorization_modal_window';
-import './js/book_modal_window';
+// import './js/book_modal_window';
 import './js/mobile_menu';
 import './js/about_us_modal_window';
 

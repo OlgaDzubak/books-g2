60210c62ada314a557818aa79a9dc2a814d3425b
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-17 19:32:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import './js/mobile_menu';
 import './js/about_us_modal_window';
 
 
-// const homeLinkEl = document.querySelector('.js-nav-homelink');
-// const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
-// homeLinkEl.classList.toggle('selected');
-// mobileHomeLinkEl.classList.toggle('mobile-selected');
+const homeLinkEl = document.querySelector('.js-nav-homelink');
+const mobileHomeLinkEl = document.querySelector('.js-mobile-nav-homelink');
+homeLinkEl.classList.toggle('selected');
+mobileHomeLinkEl.classList.toggle('mobile-selected');
 
 
 

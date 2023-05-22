55bbd9024881c5ae57ea0ef2ffa76b9394bd3268
AleksSylka <119104646+AleksSylka@users.noreply.github.com>
2023-05-22 10:11:03 +0300

    dark-modal

--- src/js/isChangeTheme.js
+++ src/js/isChangeTheme.js
@@ -1,18 +1,35 @@
 const checkBoxEl = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
-
+/* import { addDarkClassToModal } from './book_modal_window'; */
 // Фукнція що вішає слухача подій на перемикач тем і при кліку якщо є темна тема
 //  в локалсторадж - видаляє її, в усіх інших випадках - встановлює темну тему в локалсторадж
+const checkLS = localStorage.getItem('theme')
+if (checkLS === null || checkLS === '') {
+  checkBoxEl.checked = false;
+} else {
+  checkBoxEl.checked = true;
+}
+if (document.location.pathname === "/index.html") {
+    addDarkClassToModal()
+  }
+
 
 checkBoxEl.addEventListener('change', (event) => {
-    event.preventDefault();
+  event.preventDefault();
+  console.dir(checkBoxEl.checked);
     if (localStorage.getItem('theme') === 'dark') {
-    localStorage.removeItem('theme');
+      localStorage.removeItem('theme');
+      /* checkBoxEl.checked = false; */
     }
     else {
-    localStorage.setItem('theme', 'dark')
+      localStorage.setItem('theme', 'dark')
+      /* checkBoxEl.checked = true; */
     }
-    addDarkClassToHTML()
+  addDarkClassToHTML()
+  if (document.location.pathname === "/index.html") {
+    addDarkClassToModal()
+  }
+  
     });
 
   // // фукція при умові наявності в локал сторадж темної теми додає класи темної теми,
@@ -21,11 +38,11 @@ checkBoxEl.addEventListener('change', (event) => {
     export default function addDarkClassToHTML() {
    
     if (localStorage.getItem('theme') === 'dark') {
-    document.querySelector('body').classList.add('dark');
+      document.querySelector('body').classList.add('dark');
     }
     else {
-    document.querySelector('body').classList.remove('dark');
-    // document.querySelector('.themetoggle span').textContent = 'wb_sunny';
+      document.querySelector('body').classList.remove('dark');
+    // document.querySelector('.themetoggle span').textContent = 'wb_sunny'; modal-light
     }
     } 
     addDarkClassToHTML();
@@ -65,3 +82,35 @@ checkBoxEl.addEventListener('change', (event) => {
 //     }
 //   }
 //   savedThemeOnReloaded();
+
+
+
+function addDarkClassToModal() {
+    const modalBookEl = document.querySelector('.modal-container');
+    const modalMessageEl = document.querySelector('.modal-message');
+    if (localStorage.getItem('theme') === 'dark') {
+      
+      const arrTagLight = document.querySelectorAll('.modal-text-light');
+      modalBookEl.classList.remove('modal-light');
+      modalBookEl.classList.add('modal-dark');
+      modalMessageEl.classList.remove('message-light');
+      modalMessageEl.classList.add('message-dark');
+      arrTagLight.forEach((element) => {
+        element.classList.remove('modal-text-light');
+        element.classList.add('modal-text-dark');
+      })
+    }
+    else {
+      
+      const arrTagLight = document.querySelectorAll('.modal-text-dark');
+      modalBookEl.classList.remove('modal-dark');
+      modalBookEl.classList.add('modal-light');
+      modalMessageEl.classList.remove('message-dark');
+      modalMessageEl.classList.add('message-light');
+      arrTagLight.forEach((element) => {
+        element.classList.remove('modal-text-dark');
+        element.classList.add('modal-text-light');
+      })
+    // document.querySelector('.themetoggle span').textContent = 'wb_sunny'; modal-light
+    }
+    }

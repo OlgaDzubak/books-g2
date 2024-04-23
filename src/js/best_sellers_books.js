48728aed29afb4f014fabbb02477ba738e65cdce
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2024-04-23 17:43:11 +0300

    Update best_sellers_books.js

--- src/js/best_sellers_books.js
+++ src/js/best_sellers_books.js
@@ -122,11 +122,17 @@ async function loadMore(event) {
 
 // Функція що скорочує title i author на книгаг до вказаного числа символів та додає три крапки в кінці
 function shortTitle(string, value) {
-    const spaceCount = subStrCount(string, " ");
-    if(string.length > Number(value)){
-       return string.slice(0, Number(value)) + '...';
+  const spaceCount = subStrCount(string, " ");
+    
+  if(string.length > Number(value)){
+      switch (spaceCount){
+        case 0:  return string.slice(0, Number(value)) + '...';
+        case 1:  return string.slice(0, Number(value-1)) + '...';
+        default: return string.slice(0, Number(value-2)) + '...';
+      }
+    else{
+      return string;
     }
-    return string
 }
 
 // Функція, яка підраховує кількість входжень рядка subStr в str
